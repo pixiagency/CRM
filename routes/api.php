7c44d2dc734b09aafd99d14a1a8e31108f89b3ae
pixiagency <abdelqader.montaser@gmail.com>
2025-03-10 14:10:22 +0200

    remove dd(request())

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,6 @@ foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->name('central.')->group(function () {
         Route::group(['prefix' => 'authentication', 'middleware' => 'guest'], function () {
             Route::post('signup', [centralAuthController::class, 'signup'])->name('signup');
-
-            dd(request());
             Route::post('hi', fn() => 'hi');
             Route::post('login', [centralAuthController::class, 'login'])->name('login');
         });
